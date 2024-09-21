cbc129d96af3405caad8e3413bfe6e51b4fe6122
Robert P. <porobertdev@gmail.com>
2024-09-21 19:15:40 +0300

    Save cat facts to a file using `fs.appendFile`

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const { createServer } = require('node:http');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -9,6 +10,9 @@ const server = createServer( async (req, res) => {
 
     const catFact = await getCatFact();
     console.log("🚀 ~ server ~ catFact:", catFact)
+
+    // write to file
+    saveData(catFact);
 
     res.end(catFact);
 });
@@ -24,4 +28,15 @@ async function getCatFact() {
     console.log(data);
 
     return data.fact;
+}
+
+function saveData(data) {
+    // fs.appendFile() - file path, data, cb to handle error
+    fs.appendFile('./data/catFacts.txt', `\n${data}`, (err) => {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log('data saved succesfully!')
+        }
+    });
 }
