e1583dac26294818a2f79ecc3eb1282a04ca7f28
Robert P. <porobertdev@gmail.com>
2024-09-24 20:03:36 +0300

    Move mocking data to its own mock file

--- server.js
+++ server.js
@@ -1,26 +1,5 @@
 const http = require('node:http');
-
-// convert obj to JSON to be served on server requests
-const data = JSON.stringify([
-    {
-      id: "A1",
-      name: "Vacuum Cleaner",
-      rrp: "99.99",
-      info: "The most powerful vacuum in the world.",
-    },
-    {
-      id: "A2",
-      name: "Leaf Blower",
-      rrp: "303.33",
-      info: "This product will blow your socks off.",
-    },
-    {
-      id: "B1",
-      name: "Chocolate Bar",
-      rrp: "22.40",
-      info: "Delicious overpriced chocolate.",
-    },
-]);
+const mockData = require('./static/mockService');
 
 const server = http.createServer( (req, res) => {
 
@@ -34,7 +13,7 @@ const server = http.createServer( (req, res) => {
     res.writeHead(200, { 'Content-Type': 'application/json'});
 
     // send data
-    res.end(data);
+    res.end(mockData);
 });
 
 server.listen(3000, () => {
