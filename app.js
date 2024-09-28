7cab0a1a681b927d4e8de6db775ed897dc511df1
Robert P <porobertdev@gmail.com>
2024-09-28 20:53:37 +0300

    Define the port in an env variable

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ const app = express();
 // Route
 app.get('/', (req, res) => res.send('Hello world!'));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => console.log(`My first Express app - listening on port ${PORT}!`));
